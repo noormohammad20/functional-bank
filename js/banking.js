7280fe813988567a85fd47ab4fe4126776185cc0
noormohammad20 <mdsaim204@gmail.com>
2022-02-10 22:56:43 +0600

    Share one function between two input field

--- js/banking.js
+++ js/banking.js
@@ -1,21 +1,16 @@
-// function doubleIt(num) {
-//     const result = num * 2;
-//     return result;
-// }
-
-function getInputValue() {
-    const depositInput = document.getElementById('deposit-input')
-    const depositAmountText = depositInput.value
-    const depositAmount = parseFloat(depositAmountText)
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId)
+    const inputAmountText = inputField.value
+    const amountValue = parseFloat(inputAmountText)
     //clear input field
-    depositInput.value = '';
-    return depositAmount
+    inputField.value = '';
+    return amountValue
 
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
 
-    const depositAmount = getInputValue()
+    const depositAmount = getInputValue('deposit-input')
 
     //get current deposit
     const depositTotal = document.getElementById('deposit-total')
@@ -36,9 +31,8 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 // handle withdraw button
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input')
-    const withdrawAmountText = withdrawInput.value;
-    const withdrawAmount = parseFloat(withdrawAmountText)
+
+    const withdrawAmount = getInputValue('withdraw-input')
 
     // update withdraw total
     const withdrawTotal = document.getElementById('withdraw-total')
@@ -55,6 +49,4 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
 
     balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
 
-    // clear withdraw input field 
-    withdrawInput.value = '';
 });
